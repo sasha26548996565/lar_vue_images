5920dcf464e7c825f8aa89ed0603396253f05c9b
sasha <sashapozhidaev07@example.com>
2023-05-08 14:19:32 +0600

    migrate struct

--- database/migrations/2023_05_08_080705_create_images_table.php
+++ database/migrations/2023_05_08_080705_create_images_table.php
@@ -1,25 +1,23 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\Post;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('images', function (Blueprint $table) {
             $table->id();
+            $table->string('path');
+            $table->string('url');
+            $table->foreignIdFor(Post::class)->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('images');
