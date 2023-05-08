7e760ada714dc9d0ddcd6e77e403a2c360c1d522
sasha <sashapozhidaev07@example.com>
2023-05-08 14:23:09 +0600

    relations in model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,11 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title'];
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(Image::class, 'post_id', 'id');
+    }
 }
